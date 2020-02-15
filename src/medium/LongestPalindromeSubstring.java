00f056bf6ef7185a2c56f7b8891e46d49763ad44
XiaoyiHuang <HXY.Marco@gmail.com>
2020-02-15 13:14:49 +0800

    Minor style & format adjustments

--- src/medium/LongestPalindromeSubstring.java
+++ src/medium/LongestPalindromeSubstring.java
@@ -6,7 +6,8 @@ import helper.Printer;
  * [5] Longest Palindrome Substring
  *
  * Description:
- * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ * Given a string s, find the longest palindromic substring in s.
+ * You may assume that the maximum length of s is 1000.
  *
  * Example 1:
  * Input: "babad"
--- src/medium/PermutationsII.java
+++ src/medium/PermutationsII.java
@@ -4,9 +4,7 @@ import helper.Printer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * [47] Permutations II
